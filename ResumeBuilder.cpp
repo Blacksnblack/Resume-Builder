6b7be8d5aa5805cec139b7b80b69e731f0df3b39
Terry Black <blacksnblack@gmail.com>
2015-09-17 10:28:56 -0600

    Update ResumeBuilder.cpp
    Initialized some Variables and set up a couple questions for Name, age, phone number, address, and email

--- ResumeBuilder.cpp
+++ ResumeBuilder.cpp
@@ -1,18 +1,54 @@
+//
+//  main.cpp
+//  ResumeBuilder
+//
+//  Created by Terry Black on 9/17/15.
+
+
 #include <iostream>
+#include <string>
+using std::string;
 using namespace std;
 
-void main ()
-{
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
+
+int NumOfGoals;
+int NumOfSkills;
+int NumOfAwards;
+int NumOfJobs;
+string Age;
+string PhoneNumber;
+string SchoolName;
+string Degree;
+string Major;
+string Concentration;
+string Minor;
+string GradDate;
+string CommunityInvolvement;
+string Name;
+string Address;
+string Email;
+string Credentials;
+
+
+
+
+int main() {
+    
+    cout << "Hello, Welcome to Resume Builder!\n";
+    cout << "Input your name:\n";
+    getline (cin, Name);
+    cout << "How old are you?\n";
+    getline (cin, Age);
+    cout << "What is your Address?\n";
+    getline (cin, Address);
+    cout << "What is your Phone Number?\n";
+    getline (cin, PhoneNumber);
+    cout << "What is your email?\n";
+    getline (cin, Email);
+    
+    
+    
+    
+    return 0;
 }
+
