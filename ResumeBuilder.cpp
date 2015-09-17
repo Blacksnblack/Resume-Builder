c1b4fbe22fe29553cf4f474091e77b59e253165e
Terry Black <blacksnblack@gmail.com>
2015-09-17 10:58:35 -0600

    Update ResumeBuilder.cpp
    updated stuff

--- ResumeBuilder.cpp
+++ ResumeBuilder.cpp
@@ -15,10 +15,10 @@ int NumOfGoals;
 int NumOfSkills;
 int NumOfAwards;
 int NumOfJobs;
+int NumOfDegrees;
 string Age;
 string PhoneNumber;
 string SchoolName;
-string Degree;
 string Major;
 string Concentration;
 string Minor;
@@ -28,6 +28,9 @@ string Name;
 string Address;
 string Email;
 string Credentials;
+bool MajorBool;
+bool MinorBool;
+bool GradDateBool;
 
 
 
@@ -45,6 +48,32 @@ int main() {
     getline (cin, PhoneNumber);
     cout << "What is your email?\n";
     getline (cin, Email);
+    cout << "What is the name of your School?\n";
+    getline (cin, SchoolName);
+    cout << "What is your Major? If You don't have none, type: 'none'\n";
+    getline (cin, Major);
+    if (Major == "none") {
+        MajorBool = false;
+    }else {
+        MajorBool = true;
+    }
+    cout << "What is your Minor? If you don't have none, type 'none'\n";
+    getline (cin, Minor);
+    if (Minor == "none") {
+        MinorBool = false;
+    } else {
+        MinorBool = true;
+    }
+    cout << "What is your graduation date? If you are still enrolled or something, type: 'none'\n";
+    getline (cin, GradDate);
+    if (GradDate == "none") {
+        GradDateBool = false;
+    } else {
+        GradDateBool = true;
+    }
+    cout << "How many degrees do you have?\n";
+    
+    
     
     
     
